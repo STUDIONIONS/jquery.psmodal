a521e800a154782243b0fde7d86c4fd2cd135315
ProjectSoft <projectsoft2009@yandex.ru>
2018-10-26 18:39:06 +0400

    Dad, then check it out

---
+++ gruntfile.js
@@ -0,0 +1,205 @@
+module.exports = function(grunt){
+	var gc = {
+		imageNotyfy: __dirname+'\\src\\notify.png',
+		minifyHtml: false,
+		minifyCss: false,
+		dist: 'docs/assets'
+	};
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	grunt.initConfig({
+		globalConfig : gc,
+		pkg : grunt.file.readJSON('package.json'),
+		less: {
+			css: {
+				files : {
+					'test/css/main.css' : [
+						'src/less/main.less'
+					]
+				},
+				options : {
+					compress: gc.minifyCss,
+					ieCompat: false
+				}
+			}
+		},
+		autoprefixer:{
+			options: {
+				browsers: ['last 2 versions', 'Android 4', 'ie 8', 'ie 9', 'Firefox >= 27', 'Opera >= 12.0', 'Safari >= 6'],
+				cascade: false
+			},
+			css: {
+				expand: true,
+				flatten: true,
+				src: [
+					'test/css/main.css'
+				],
+				dest: 'test/css/main_pref/'//'assets/templates/skat_<%= pkg.version%>/css/'
+			}
+		},
+		cssmin: {
+			target: {
+				files: {
+					'<%= globalConfig.dist%>/css/main.css': [
+						'test/css/main_pref/main.css'
+					]
+				}
+			}
+		},
+		jshint: {
+			src: [
+				'src/js/jquery.psmodal.js',
+				'src/js/main.js'
+			],
+		},
+		uglify : {
+			options: {
+				ASCIIOnly: true,
+				//beautify: true
+			},
+			main: {
+				files: {
+					'<%= globalConfig.dist%>/js/jquery.js': [
+						'bower_components/jquery/dist/jquery.js',
+					],
+					'<%= globalConfig.dist%>/js/main.js': [
+						'src/js/main.js',
+					],
+					'<%= globalConfig.dist%>/js/jquery.psmodal.js': [
+						'src/js/jquery.psmodal.js',
+					]
+				}
+			}
+		},
+		imagemin: {
+			base: {
+				options: {
+					optimizationLevel: 7,
+					//progressive: true,
+					//interlaced: true,
+					svgoPlugins: [
+						{
+							removeViewBox: false
+						}
+					]
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'src/images/',
+						src: ["**/*.{png,jpg,jpeg,gif,svg}"],
+						dest: 'test/images/'
+					},
+				]
+			},
+		},
+		tinyimg: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'test/images/', 
+					src: ['**/*.{png,jpg,jpeg,svg}'],
+					dest: '<%= globalConfig.dist%>/images/'
+				}]
+			}
+		},
+		pug: {
+			files: {
+				options: {
+					pretty: '\t',
+					separator:  '\n'
+				},
+				files: {
+					"index.html": ['src/pug/index.pug'],
+				}
+			}
+		},
+		/*
+		jade: {
+			files: {
+				options: {
+					pretty: !gc.minifyHtml,
+					data: {
+						debug: false
+					}
+				},
+				files: {
+					"index.php": [
+						"src/jade/index.jade"
+					],
+				}
+			}
+		},
+		*/
+		watch: {
+			options: {
+				livereload: true,
+			},
+			html: {
+				files: [
+					'src/pug/**/*.php',
+					'src/pug/**/*.pug',
+				],
+				tasks: ["pug","notify:done"]
+			},
+			js: {
+				files: [
+					'src/js/**/*.js'
+				],
+				tasks: [
+					'notify:watch',
+					'jshint',
+					'uglify',
+					'notify:done'
+				]
+			},
+			css: {
+				files: [
+					'src/less/**/*.{css,less}',
+				],
+				tasks: ['notify:watch', 'less', 'autoprefixer','cssmin','notify:done']
+			},
+			images: {
+				files: [
+					'src/images/*.{png,jpg,jpeg,gif,svg}',
+				],
+				tasks: ['notify:watch', 'imagemin', 'tinyimg', 'notify:done']//
+			}
+		},
+		notify: {
+			watch: {
+				options: {
+					title: "<%= pkg.name %> v<%= pkg.version %>",
+					message: 'Запуск',
+					image: '<%= globalConfig.imageNotyfy %>'
+				}
+			},
+			done: {
+				options: { 
+					title: "<%= pkg.name %> v<%= pkg.version %>",
+					message: "Успешно Завершено",
+					image: '<%= globalConfig.imageNotyfy %>'
+				}
+			}
+		}
+	});
+	grunt.registerTask('default',
+		[
+			'notify:watch',
+			'imagemin',
+			'tinyimg',
+			'less',
+			'autoprefixer',
+			'cssmin',
+			'jshint',
+			'uglify',
+			'pug',
+			'notify:done'
+		]
+	);
+	grunt.registerTask('dev',
+		[
+			'watch'
+		]
+	);
+};
